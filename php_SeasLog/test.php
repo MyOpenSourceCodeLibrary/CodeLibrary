4b269e3d9d7c3dc92ac45680127767e4a4d83fea
liangbob <Bob.liang@yunzhihui.com>
2015-12-17 17:17:57 +0800

    seaslog体验

---
+++ php_SeasLog/test.php
@@ -0,0 +1,21 @@
+<?php
+SeasLog::setLogger('bobtest');//设置模块的名称
+SeasLog::setBasePath('./');//设置日志生成目录的路径
+
+SeasLog::log(SEASLOG_ERROR,'this is a error test by ::log');
+
+SeasLog::debug('this is a {userName} debug',array('{userName}' => 'neeke'));
+
+SeasLog::info('this is a info log');
+
+SeasLog::notice('this is a notice log');
+
+SeasLog::warning('your {website} was down,please {action} it ASAP!',array('{website}' => 'github.com','{action}' => 'rboot'));
+
+SeasLog::error('a error log');
+
+SeasLog::critical('some thing was critical');
+
+SeasLog::alert('yes this is a {messageName}',array('{messageName}' => 'alertMSG'));
+
+SeasLog::emergency('Just now, the house next door was completely burnt out! {note}',array('{note}' => 'it`s a joke'));
